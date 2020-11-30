2eb3c281adcf2d08390a71707ab1befac1b6d720
Denise Limon Dev <deniselimong.dev@gmail.com>
2020-11-29 23:15:03 -0600

    ModelVO

---
+++ src/main/java/com/frontier/back/lemon_tree/biz/vo/ModelVO.java
@@ -0,0 +1,33 @@
+package com.frontier.back.lemon_tree.biz.vo;
+
+public class ModelVO {
+    private int  id;
+
+    private int created_at;
+
+    private int updated_at;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(int created_at) {
+        this.created_at = created_at;
+    }
+
+    public int getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(int updated_at) {
+        this.updated_at = updated_at;
+    }
+}
