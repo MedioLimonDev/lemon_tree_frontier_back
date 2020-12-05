41e6d7586a99ba53c90b730ebb582dc4dd4a4856
Denise Limon Dev <deniselimong.dev@gmail.com>
2020-12-05 01:12:40 -0600

    Se agrega clase RecordVO

---
+++ src/main/java/com/frontier/back/lemon_tree/biz/vo/RecordVO.java
@@ -0,0 +1,23 @@
+package com.frontier.back.lemon_tree.biz.vo;
+
+public class RecordVO extends ModelVO {
+    private int idEmpleyee;
+
+    private int done;
+
+    public int getIdEmpleyee() {
+        return idEmpleyee;
+    }
+
+    public void setIdEmpleyee(int idEmpleyee) {
+        this.idEmpleyee = idEmpleyee;
+    }
+
+    public int getDone() {
+        return done;
+    }
+
+    public void setDone(int done) {
+        this.done = done;
+    }
+}
